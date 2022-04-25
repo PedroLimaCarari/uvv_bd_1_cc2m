f9afc540a87b8d3dc3f20591402c9006177db18b
PedroLimaCarari <95299280+PedroLimaCarari@users.noreply.github.com>
2022-04-25 18:00:05 -0300

    Update mariaDB_1.sql

--- pset1/scripts/mariaDB_1.sql
+++ pset1/scripts/mariaDB_1.sql
@@ -1,7 +1,9 @@
--- Primeiro criaremos o usuário.
+-- Primeiro criaremos o usuário para gerenciar o banco de dados.
 CREATE USER 'pedro'@'localhost' IDENTIFIED BY 1234;
+
 -- Depois criaremos o banco de dados da uvv.
 CREATE DATABASE uvv CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- Agora garantir todos os privilégios ao usuário.
 GRANT ALL PRIVILEGES ON uvv.* to 'pedro'@'localhost'; 
 SYSTEM mysql -pedro -p;
