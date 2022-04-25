bf2b6a4b901b8e4fb10373849004416dd962214d
PedroLimaCarari <95299280+PedroLimaCarari@users.noreply.github.com>
2022-04-25 18:00:30 -0300

    Update mariaDB_1.sql

--- pset1/scripts/mariaDB_1.sql
+++ pset1/scripts/mariaDB_1.sql
@@ -6,4 +6,4 @@ CREATE DATABASE uvv CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Agora garantir todos os privilégios ao usuário.
 GRANT ALL PRIVILEGES ON uvv.* to 'pedro'@'localhost'; 
-SYSTEM mysql -pedro -p;
+SYSTEM mysql -u pedro -p;
